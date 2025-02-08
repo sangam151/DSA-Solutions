6e6f9f29e62efa1786e0104f9e6516fb421fd601
sangam <sangampoudel560@gmail.com>
2025-02-08 21:40:18 +0545

    removing duplicate element of the array using optimal solution

---
+++ PROBLEMS ON ARRAY/problem4_mtd2.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int remove_duplicate(int arr[],int n)
+{
+    int i=0;
+    for(int j=1;j<n;j++)
+    {
+        if(arr[i]!=arr[j])
+        {
+            i++;
+            arr[i]=arr[j];
+        }
+
+    }
+    return i+1;
+}
+int main(){
+        int arr[]={1,1,2,2,3,3,3};
+        int n=sizeof(arr)/sizeof(arr[0]);
+        int k=remove_duplicate(arr,n);
+        cout<<"array after removing duplicate"<<endl;
+        for(int i=0;i<k;i++)
+        {
+            cout<<arr[i]<<" "<<endl;
+        }
+    return 0;
+}
