fdc496d8ee4d8ca3e75fbf378d81ed0b2564a294
sangam <sangampoudel560@gmail.com>
2025-05-08 22:59:57 +0545

    adding binary search problems

---
+++ BINARY SEARCH/problem1.cpp
@@ -0,0 +1,7 @@
+ //Binary search to find X in the sorted array iterative implementation
+ #include<bits/stdc++.h>
+ using namespace std;
+ int main()
+ {
+    printf("hello world");
+ }
---
+++ BINARY SEARCH/problem1_mtd2.cpp
@@ -0,0 +1,30 @@
+ //Binary search to find X in the sorted array iterative implementation
+ #include<bits/stdc++.h>
+ using namespace std;
+ int binarysearch(vector<int> a,int target,int low,int high)
+ {
+    
+    
+    int mid=(low+high)/2;
+    if(a[mid]==target)return mid;
+    else if(target>a[mid]) return binarysearch(a,target,mid+1,high);
+    else return binarysearch(a,target,low,mid-1);
+    return -1;
+    
+ }
+ int main()
+ {
+    vector<int> a={3,4,6,7,9,12,16,17};
+    int target=6;
+    int n=a.size();
+
+    int ind=binarysearch(a,target,0,n-1);
+    if(ind==-1)cout<<"The target is not present"<<endl;
+    else
+    cout<<"The target is at index "<<ind<<endl;
+
+
+    return 0;
+   
+ }
+ 
