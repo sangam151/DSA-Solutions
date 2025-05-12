e9ffcd43c6aadb05f79715ec91f771b699ba7d5c
sangam <sangampoudel560@gmail.com>
2025-05-12 07:54:57 +0545

    minimun in totated sorted array

---
+++ BINARY SEARCH/problem11_mtd2.cpp
@@ -0,0 +1,37 @@
+// minimum in rotated sorted array(optimization)
+#include <bits/stdc++.h>
+using namespace std;
+int minSorted(vector<int> &arr)
+{
+    int n = arr.size();
+    int ans = INT_MAX;
+    int low = 0;
+    int high = n - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if(arr[low]<arr[high])
+        {
+            ans=min(ans,arr[low]);
+            break;
+        }
+        if (arr[low] <= arr[mid])
+        {
+            ans = min(ans, arr[low]);
+            low = mid + 1;
+        }
+        else
+        {
+            ans = min(ans, arr[mid]);
+            high=mid-1;
+        }
+    }
+    return ans;
+}
+int main()
+{
+    vector<int> arr = {4, 5, 6, 7, 0, 1, 2, 3};
+    int ans = minSorted(arr);
+    cout << "The minimum in sorted array is: " << ans << endl;
+    return 0;
+}
