d8850d9eec2f261ff1721ce24646d88581d80280
sangam <sangampoudel560@gmail.com>
2025-06-20 11:10:27 +0545

    kahn's algorithm

---
+++ GRAPHS/problem14.cpp
@@ -0,0 +1,53 @@
+//kahn's algorithm
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    // function to return list containing vertices in Topological order
+    vector<int> topoSort(int V, vector<int> adj[])
+    {
+        int indegree[V] = {0};
+        for (int i = 0; i < V; i++)
+        {
+            for (auto it : adj[i])
+            {
+                indegree[it]++;
+            }
+        }
+        queue<int> q;
+        for (int i = 0; i < V; i++)
+        {
+            if (indegree[i] == 0)
+            {
+                q.push(i);
+            }
+        }
+        vector<int> topo;
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for (auto it : adj[node])
+            {
+                indegree[it]--;
+                if (indegree[it] == 0)
+                    q.push(it);
+            }
+        }
+        return topo;
+    }
+};
+int main()
+{
+    vector<int> adj[6] = {{}, {}, {3}, {1}, {0, 1}, {0, 2}};
+    int V = 6;
+    Solution obj;
+    vector<int> ans = obj.topoSort(V, adj);
+    for (auto it : ans)
+    {
+        cout << it << " ";
+    }
+    return 0;
+}
