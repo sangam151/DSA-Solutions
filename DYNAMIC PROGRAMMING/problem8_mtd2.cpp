c320d209013cd1c673e3f8877024879ba48536ea
sangam <sangampoudel560@gmail.com>
2026-01-12 18:34:53 +0545

    Grid Unique path by tabulation approach

---
+++ DYNAMIC PROGRAMMING/problem8_mtd2.cpp
@@ -0,0 +1,42 @@
+// Grid unique path by tabulation approach || DP problem
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+    int func(int m, int n, vector<vector<int>> &dp)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i == 0 && j == 0)
+                {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                int up = 0;
+                int left = 0;
+                if (i > 0)
+                    up = dp[i - 1][j];
+                if (j > 0)
+                    left = dp[i][j - 1];
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+public:
+    int uniquePath(int m, int n)
+    {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return func(m, n, dp);
+    }
+};
+int main()
+{
+    int m = 3, n = 2;
+    Solution sol;
+    cout << "Number of way: " << sol.uniquePath(m, n) << endl;
+    return 0;
+}
