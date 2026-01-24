073bf5b98c26ce5c15f41012d4c8cc8ddc5a6ef9
sangam <sangampoudel560@gmail.com>
2026-01-24 13:39:28 +0545

    Unbounded knapsack by memoization approach || DP problem

---
+++ DYNAMIC PROGRAMMING/problem27.cpp
@@ -0,0 +1,38 @@
+// unbounded KnapSack by memoization approach
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+    int knapsackUtil(vector<int> &wt, vector<int> &val, int ind, int W, vector<vector<int>> &dp)
+    {
+        if (ind == 0)
+        {
+            return (W / wt[0]) * val[0];
+        }
+        if (dp[ind][W] != -1)
+            return dp[ind][W];
+        int notTaken = knapsackUtil(wt, val, ind - 1, W, dp);
+        int taken = INT_MIN;
+        if (wt[ind] <= W)
+        {
+            taken = val[ind] + knapsackUtil(wt, val, ind, W - wt[ind], dp);
+        }
+        return dp[ind][W] = max(notTaken, taken);
+    }
+
+public:
+    int unboundedKnapsack(int n, int W, vector<int> &val, vector<int> &wt)
+    {
+        vector<vector<int>> dp(n, vector<int>(W + 1, -1));
+        return knapsackUtil(wt, val, n - 1, W, dp);
+    }
+};
+int main()
+{
+    vector<int> wt = {2, 4, 6};
+    vector<int> val = {5, 11, 13};
+    int W = 10;
+    int n = wt.size();
+    Solution obj;
+    cout << "The maximum value of items the thief can steal is: " << obj.unboundedKnapsack(n, W, val, wt) << endl;
+}
