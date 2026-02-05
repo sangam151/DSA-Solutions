1cc7a653e862387df696892bae52b49f22557656
sangam <sangampoudel560@gmail.com>
2026-02-05 18:26:39 +0545

    Burst the balloons partition by tabulation approach || DP problem

---
+++ DYNAMIC PROGRAMMING/problem31_mtd2.cpp
@@ -0,0 +1,39 @@
+// Burst the balloons partition by tabulation approach
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int maxCoins(vector<int> &nums)
+    {
+        int n = nums.size();
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
+        for (int i = n; i >= 1; i--)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (i > j)
+                    continue;
+                int maxi = INT_MIN;
+                for (int ind = i; ind <= j; ind++)
+                {
+                    int coins = nums[i - 1] * nums[ind] * nums[j + 1];
+                    int remainingCoins = dp[i][ind - 1] + dp[ind + 1][j];
+                    maxi = max(maxi, coins + remainingCoins);
+                }
+                dp[i][j] = maxi;
+            }
+        }
+        return dp[1][n];
+    }
+};
+int main()
+{
+    vector<int> nums = {3, 1, 5, 8};
+    Solution sol;
+    int maxCoinsResult = sol.maxCoins(nums);
+    cout << "Maximum coins obtained: " << maxCoinsResult << "\n";
+    return 0;
+}
