ca190defcd0f45fbb29651d910ea3e12df4974c7
sangam <sangampoudel560@gmail.com>
2026-02-24 16:15:38 +0545

    Sum of subarray ranges by optimal approach

---
+++ Stack and Queue/problem20_mtd2.cpp
@@ -0,0 +1,124 @@
+// sum of subarray ranges
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+private:
+    vector<int> findNSE(vector<int> &arr)
+    {
+        int n = arr.size();
+        vector<int> ans(n);
+        stack<int> st;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            int currEle = arr[i];
+            while (!st.empty() && arr[st.top()] >= arr[i])
+            {
+                st.pop();
+            }
+            ans[i] = !st.empty() ? st.top() : n;
+            st.push(i);
+        }
+        return ans;
+    }
+    vector<int> findNGE(vector<int> &arr)
+    {
+        int n = arr.size();
+        vector<int> ans(n);
+        stack<int> st;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            int currEle = arr[i];
+            while (!st.empty() && arr[st.top()] <= arr[i])
+            {
+                st.pop();
+            }
+            ans[i] = !st.empty() ? st.top() : n;
+            st.push(i);
+        }
+        return ans;
+    }
+    vector<int> findPSEE(vector<int> &arr)
+    {
+        int n = arr.size();
+        vector<int> ans(n);
+        stack<int> st;
+        for (int i = 0; i < n; i++)
+        {
+            int currEle = arr[i];
+            while (!st.empty() && arr[st.top()] > arr[i])
+            {
+                st.pop();
+            }
+            ans[i] = !st.empty() ? st.top() : -1;
+            st.push(i);
+        }
+        return ans;
+    }
+    vector<int> findPGEE(vector<int> &arr)
+    {
+        int n = arr.size();
+        vector<int> ans(n);
+        stack<int> st;
+        for (int i = 0; i < n; i++)
+        {
+            int currEle = arr[i];
+            while (!st.empty() && arr[st.top()] < arr[i])
+            {
+                st.pop();
+            }
+            ans[i] = !st.empty() ? st.top() : -1;
+            st.push(i);
+        }
+        return ans;
+    }
+    int sumSubarrayMins(vector<int> &arr)
+    {
+        vector<int> nse = findNSE(arr);
+        vector<int> psee = findPSEE(arr);
+        int n = arr.size();
+        int mode = 1e9 + 7;
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int left = i - psee[i];
+            int right = nse[i] - i;
+            long long freq = left * right * 1LL;
+            int val = (freq * arr[i] * 1LL) % mode;
+            sum = (sum + val) % mode;
+        }
+        return sum;
+    }
+    int sumSubarrayMaxs(vector<int> &arr)
+    {
+        vector<int> nge = findNGE(arr);
+        vector<int> pgee = findPGEE(arr);
+        int n = arr.size();
+        int mode = 1e9 + 7;
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int left = i - pgee[i];
+            int right = nge[i] - i;
+            long long freq = left * right * 1LL;
+            int val = (freq * arr[i] * 1LL) % mode;
+            sum = (sum + val) % mode;
+        }
+        return sum;
+    }
+
+public:
+    long long subArrayRanges(vector<int> &arr)
+    {
+        return (sumSubarrayMaxs(arr) -
+                sumSubarrayMins(arr));
+    }
+};
+int main()
+{
+    vector<int> arr = {1,2,3};
+    Solution sol;
+    int ans = sol.subArrayRanges(arr);
+    cout << "The sum of minimum value in each subarray is: " << ans;
+    return 0;
+}
