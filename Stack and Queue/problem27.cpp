b156e7a98c89cc83ac404b50376cc66cd23cba3f
sangam <sangampoudel560@gmail.com>
2026-02-26 21:46:12 +0545

    LFU(list frquently used)

---
+++ Stack and Queue/problem27.cpp
@@ -0,0 +1,143 @@
+// LFU(Least frequently used)
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int key, value, cnt;
+    Node *next;
+    Node *prev;
+    Node(int _key, int _val)
+    {
+        key = _key;
+        value = _val;
+        cnt = 1;
+    }
+};
+struct List
+{
+    int size;
+    Node *head;
+    Node *tail;
+    List()
+    {
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head->next = tail;
+        tail->prev = head;
+        size = 0;
+    }
+    void addFront(Node *node)
+    {
+        Node *temp = head->next;
+        node->next = temp;
+        node->prev = head;
+        head->next = node;
+        temp->prev = node;
+        size++;
+    }
+    void removeNode(Node *delNode)
+    {
+        Node *prevNode = delNode->prev;
+        Node *nextNode = delNode->next;
+        prevNode->next = nextNode;
+        nextNode->prev = prevNode;
+        size--;
+    }
+};
+class LFUcache
+{
+private:
+    map<int, Node *> keyNode;
+    map<int, List *> freqListMap;
+    int maxSizeCache;
+    int minFreq;
+    int curSize;
+
+public:
+    LFUcache(int capacity)
+    {
+        maxSizeCache = capacity;
+        minFreq = 0;
+        curSize = 0;
+    }
+    void updateFreqListMap(Node *node)
+    {
+        keyNode.erase(node->key);
+        freqListMap[node->cnt]->removeNode(node);
+        if (node->cnt == minFreq && freqListMap[node->cnt]->size == 0)
+        {
+            minFreq++;
+        }
+        List *nextHigherFreqList = new List();
+        if (freqListMap.find(node->cnt + 1) != freqListMap.end())
+        {
+            nextHigherFreqList = freqListMap[node->cnt + 1];
+        }
+        node->cnt += 1;
+        nextHigherFreqList->addFront(node);
+        freqListMap[node->cnt] = nextHigherFreqList;
+        keyNode[node->key] = node;
+    }
+    int get(int key)
+    {
+        if (keyNode.find(key) != keyNode.end())
+        {
+            Node *node = keyNode[key];
+            int val = node->value;
+            updateFreqListMap(node);
+            return val;
+        }
+        return -1;
+    }
+    void put(int key, int value)
+    {
+        if (maxSizeCache == 0)
+            return;
+        if (keyNode.find(key) != keyNode.end())
+        {
+            Node *node = keyNode[key];
+            node->value = value;
+            updateFreqListMap(node);
+        }
+        else
+        {
+            if (curSize == maxSizeCache)
+            {
+                List *list = freqListMap[minFreq];
+                keyNode.erase(list->tail->prev->key);
+                freqListMap[minFreq]->removeNode(list->tail->prev);
+                curSize--;
+            }
+            curSize++;
+            minFreq = 1;
+            List *listFreq = new List();
+            if (freqListMap.find(minFreq) != freqListMap.end())
+            {
+                listFreq = freqListMap[minFreq];
+            }
+            Node *node = new Node(key, value);
+            listFreq->addFront(node);
+            keyNode[key] = node;
+            freqListMap[minFreq] = listFreq;
+        }
+    }
+};
+int main()
+{
+    // LFU Cache
+    LFUcache cache(2);
+
+    // Queries
+    cache.put(1, 1);
+    cache.put(2, 2);
+    cout << cache.get(1) << " ";
+    cache.put(3, 3);
+    cout << cache.get(2) << " ";
+    cout << cache.get(3) << " ";
+    cache.put(4, 4);
+    cout << cache.get(1) << " ";
+    cout << cache.get(3) << " ";
+    cout << cache.get(4) << " ";
+
+    return 0;
+}
