9a9782071a985e09a57d7bb20617a5e98a03a182
sangam <sangampoudel560@gmail.com>
2026-03-02 13:14:52 +0545

    Morris preorder traversal of binary tree

---
+++ BINARY TREE/problem28.cpp
@@ -0,0 +1,65 @@
+// Morris preorder traversal of binary tree
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+class Solution
+{
+public:
+    vector<int> getpreorder(TreeNode *root)
+    {
+        vector<int> preorder;
+        TreeNode *cur = root;
+        while (cur != NULL)
+        {
+            if (cur->left == NULL)
+            {
+                preorder.push_back(cur->val);
+                cur = cur->right;
+            }
+            else
+            {
+                TreeNode *prev = cur->left;
+                while (prev->right && prev->right != cur)
+                {
+                    prev = prev->right;
+                }
+                if (prev->right == NULL)
+                {
+                    prev->right = cur;
+                    preorder.push_back(cur->val);
+                    cur = cur->left;
+                }
+                else
+                {
+                    prev->right = NULL;
+                    cur = cur->right;
+                }
+            }
+        }
+        return preorder;
+    }
+};
+int main()
+{
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->left->right->right = new TreeNode(6);
+    Solution sol;
+    vector<int> inorder = sol.getpreorder(root);
+    cout << "Binary Tree Morris Inorder Traversal: ";
+    for (int i = 0; i < inorder.size(); i++)
+    {
+        cout << inorder[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
