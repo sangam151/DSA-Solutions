002eec749385c40458134369e7fb861222955b61
sangam <sangampoudel560@gmail.com>
2026-03-25 23:18:29 +0545

    Kth largest element in the array

---
+++ HEAPS/problem3.cpp
@@ -0,0 +1,64 @@
+// Kth largest or smallest element in an array
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int kthLargestElement(vector<int> &nums, int k)
+    {
+        if (k > nums.size())
+            return -1;
+        int left = 0, right = nums.size() - 1;
+        while (true)
+        {
+            int pivotIndex = randomIndex(left, right);
+            pivotIndex = partitionAndReturnIndex(nums, pivotIndex, left, right);
+            if (pivotIndex == k - 1)
+                return nums[pivotIndex];
+            else if (pivotIndex > k - 1)
+                right = pivotIndex - 1;
+            else
+                left = pivotIndex + 1;
+        }
+        return -1;
+    }
+
+private:
+    int randomIndex(int &left, int &right)
+    {
+        int len = right - left + 1;
+        return (rand() % len) + left;
+    }
+    int partitionAndReturnIndex(vector<int> &nums, int pivotIndex, int left, int right)
+    {
+        int pivot = nums[pivotIndex];
+        swap(nums[left],nums[pivotIndex]);
+        int ind=left+1;
+        for (int i = left+1; i <= right; i++)
+        {
+            if (nums[i] > pivot)
+            {
+                swap(nums[ind], nums[i]);
+                ind++;
+            }
+        }
+        swap(nums[left],nums[ind-1]);
+        return ind-1;
+    }
+};
+int main()
+{
+    vector<int> nums = {-5, 4, 1, 2, -3};
+    int k = 5;
+
+    // Creating an object of the Solution class
+    Solution sol;
+
+    // Function call to get the Kth largest element
+    int ans = sol.kthLargestElement(nums, k);
+
+    // Output array
+    cout << "The Kth largest element in the array is: " << ans << endl;
+
+    return 0;
+}
