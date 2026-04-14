1394fe5e46491259c54ca7bc0fe2ccd5a6b2da3a
sangam <sangampoudel560@gmail.com>
2026-04-14 09:37:02 +0545

    Print longest common Subsequence || DP problem

---
+++ DYNAMIC PROGRAMMING/problem36.cpp
@@ -0,0 +1,56 @@
+// Print Longest common Subsequence
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    string longestCommonSubsequence(string &text1, string &text2)
+    {
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (text1[i - 1] == text2[j - 1])
+                {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        int i = n, j = m;
+        string lcs = "";
+        while (i > 0 && j > 0)
+        {
+            if (text1[i - 1] == text2[j - 1])
+            {
+                lcs += text1[i - 1];
+                i--;
+                j--;
+            }
+            else if (dp[i - 1][j] > dp[i][j - 1])
+            {
+                i--;
+            }
+            else
+            {
+                j--;
+            }
+        }
+        reverse(lcs.begin(), lcs.end());
+        return lcs;
+    }
+};
+int main()
+{
+    string s1 = "abcde";
+    string s2 = "ace";
+    Solution sol;
+    cout << "LCS: " << sol.longestCommonSubsequence(s1, s2) << endl;
+    return 0;
+}
